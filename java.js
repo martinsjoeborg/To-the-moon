3704e988401a1fddcc74f9a5b8bc0067488d82e1
martinsjoeborg <113284827+martinsjoeborg@users.noreply.github.com>
2022-09-26 16:40:50 +0200

    uppdatering

--- java.js
+++ java.js
@@ -4,4 +4,6 @@
     menu_btn.addEventListener('click', function () {
         menu_btn.classList.toggle('is-active');
         mobile_menu.classList.toggle('is-active');
-    });
+    
+        }
+        );
